ece565da8e42b93ea29c20e59509a5190baad3c1
Djimy <dzhamal.kichukov@gmail.com>
2023-03-27 00:00:44 +0300

    Hardcoded some users with different roles. Started SecurityConfig - configuration

--- src/main/java/com/security/userservice/model/User.java
+++ src/main/java/com/security/userservice/model/User.java
@@ -1,12 +1,17 @@
 package com.security.userservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
